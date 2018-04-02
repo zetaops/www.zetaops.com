3a27c766b4be84dd7cee95397a314a55b7b42be0
Shubidubapp <shubidubapp@gmail.com>
2018-04-02 11:36:11 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+// Load Plugins
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat')
+
+// Styles
+gulp.task('styles', function () {
+  return gulp.src('scss/theme.scss')
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
+})
+
+// Scripts
+gulp.task('scripts', function () {
+  return gulp.src([
+      'js/top/*.js',
+      'js/bootstrap/*.js',
+      'js/vendor/*.js',
+      'js/theme.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('theme.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
+})
+
+// Images
+gulp.task('images', function () {
+  gulp.src('images/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+})
+
+// Fonts
+gulp.task('fonts', function () {
+  return gulp.src('fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+// Default task
+gulp.task('default', function () {
+  gulp.start('styles', 'scripts', 'images', 'fonts')
+})
+
+// Watch
+gulp.task('watch', function () {
+  // Watch .scss files
+  gulp.watch(['scss/**/*.scss'], ['styles'])
+
+  // Watch .js files
+  gulp.watch('js/**/*.js', ['scripts'])
+})
